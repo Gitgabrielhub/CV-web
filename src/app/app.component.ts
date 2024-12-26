4a9a00c4968aa6d80cc604de83584200030eae42
gabriel <gabrielsillva356@gmail.com>
2024-12-26 02:55:46 -0300

    alteração no cabecalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ HomeComponent, HeaderComponent, RouterOutlet],
+  imports: [ HomeComponent, HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
