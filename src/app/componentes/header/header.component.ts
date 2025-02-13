d9c5110f57192f4fd70b51a87b3f9e3138eaf61c
gabriel fonseca <gabrielsillva356@gmail.com>
2025-02-12 22:36:01 -0300

    contabilizando a quantidade de clicks no open cv

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  downloadPdf():void{
+  downloadPdf(e:any):void{
+    e.preventDefault();
     const link = document.createElement('a');
     link.href = "https://docs.google.com/document/d/1Rp8cqfo7L35fIt56rgfrxTiZwhKMu49y/edit";
     link.download = "https://docs.google.com/document/d/1Rp8cqfo7L35fIt56rgfrxTiZwhKMu49y/edit";
     link.click();
+    let button = document.getElementById("button");
+    link?.setAttribute('target','_blank');
+    let clicks = 0;
+    clicks++;
+    console.log(clicks)
+    localStorage.setItem('clicks', clicks.toString());
   }
 }
